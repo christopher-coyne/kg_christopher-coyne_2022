4ed62177cfc599d5d450818b2d6dcbc68fd5ce70
Christopher Coyne <christophercoyne@ChristohersAir2.fios-router.home>
2021-05-01 09:01:32 -0400

    added command line arguments, basic looping through args

---
+++ main.js
@@ -0,0 +1,9 @@
+let num_dict = {0: "Zero", 1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 6: "Six",
+7: "Seven", 8: "Eight", 9: "Nine"}
+
+let args = process.argv.slice(2)
+
+args.forEach((val, ind) => {
+  console.log(val);
+})
+
