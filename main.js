bd1fdda8636a8dbcb356ea414e39857fd0fb3521
Christopher Coyne <christophercoyne@ChristohersAir2.fios-router.home>
2021-05-01 09:10:38 -0400

    complete: final string formatted in one line with commas

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-let num_dict = {0: "Zero", 1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 6: "Six",
+let numDict = {0: "Zero", 1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 6: "Six",
 7: "Seven", 8: "Eight", 9: "Nine"}
 
 let args = process.argv.slice(2)
 
+let finalStr = ""
 args.forEach((val, ind) => {
+  let numStr = ""
   for(const digit of val) {
-    console.log(num_dict[digit])
+    numStr += numDict[digit]
   }
+  finalStr += `${numStr},`
 })
+
+console.log(finalStr)
 
